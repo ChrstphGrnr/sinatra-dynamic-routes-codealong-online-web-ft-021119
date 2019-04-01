6affc5d569f11e45d15ebae253a02e2f1998c94b
Christoph Gruener <christophgruener@me.com>
2019-04-01 22:10:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end
 
-  get "/multiply/:num1/:num2" do
+  get '/multiply/:num1/:num2' do
     @value = params[:num1] * params[:num2]
     "#{@value}"
   end
